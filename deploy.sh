8e5227e308df9b1236d4d572d7499d1040a44d6d
jihed haj ali <jhajali@crystalchain.io>
2024-05-29 16:58:09 +0200

    add deployment script and rename docker container

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git pull
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 173367659232.dkr.ecr.eu-west-1.amazonaws.com
+docker build -t crystalchain/data_filter .
+docker tag crystalchain/data_filter:latest 173367659232.dkr.ecr.eu-west-1.amazonaws.com/crystalchain/data_filter:latest
+docker push 173367659232.dkr.ecr.eu-west-1.amazonaws.com/crystalchain/data_filter:latest
+docker compose down &&docker compose pull && docker compose up -d
