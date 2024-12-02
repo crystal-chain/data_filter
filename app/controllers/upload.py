7d68b70f43fdc24203240f704ace49e9b7815036
AmirDhouib2001 <dhouibamir9@gmail.com>
2024-12-02 15:42:53 +0000

    test

--- app/controllers/upload.py
+++ app/controllers/upload.py
@@ -15,10 +15,10 @@ upload_blueprint = Blueprint('upload', __name__)
 def select_page():
     return render_template('select_page.html')
 
-@upload_blueprint.route('/medor', methods=['GET','POST'])
+@upload_blueprint.route('/medor', methods=['GET', 'POST'])
 def upload_file_medor():
     if request.method == 'POST':
-        if 'file' not in request.files :
+        if 'file' not in request.files:
             return render_template('upload.html', error_message='Aucun fichier téléchargé.')
         file = request.files['file']
         if file.filename == '':
@@ -31,67 +31,65 @@ def upload_file_medor():
                 df = rename_medor(df)
                 # Modifier le champ _ErrorMessage en ErrorType
                 df = changer_errormessage(df)
-                # Cree le CSV de KPI
-                df_kpi=count_errors_by_type_and_manufacturer(df)
-                
-
+                # Crée le CSV de KPI
+                df_kpi = count_errors_by_type_and_manufacturer(df)
                 # Supprimer les lignes vides et colonnes vides 
                 df = nettoyer_ligne_colonne(df)
-
                 # Ajouter la colonne DataQualityType
                 df = ajouter_data_quality_type(df)
-
                 # Ajouter la colonne ErrorStatus
                 df['ErrorStatus'] = 'on going'
 
-                # Filtrer les données pour chaque type d'erreur
-                df_logic_duplicate = df[df['ErrorType'].str.startswith('Logical Duplicate with')]
-                df_perfect_duplicate = df[df['ErrorType'].str.startswith('Perfect Duplicate with')]
-                df_missing_relationship = df[df['ErrorType'].str.startswith('Missing relationship')]
-
-                # Ajouter les colonnes et supprimer les colonnes inutiles
-                df_logic_duplicate = add_columns_and_remove(df_logic_duplicate)
-                df_perfect_duplicate = add_columns_and_remove(df_perfect_duplicate)
-                df_missing_relationship = add_columns_and_remove(df_missing_relationship)
-                print(f"Colonnes après le chargement du fichier: {df.columns.tolist()}")
-                print(f"Taille du DataFrame après le chargement: {df.shape}") 
-                # Classifier les types d'erreur dans une colonne spécifiée 
-                df_missing_relationship = sort_missing_relationships(df_missing_relationship.copy())  
-                
-                # Garder la première occurrence pour les Missing relationship
-                df_missing_relationship = keep_first_occurrence_for_missing_relationship(df_missing_relationship,"ParentId")
-                # Traduction du message de log par quelque chose de plus intelligible par le client 
-                df_missing_relationship=modify_error_type(df_missing_relationship)
-
-                
-                # Ajout de la date d'aujourd'hui
+                # Ajouter la date d'aujourd'hui
                 today = datetime.today().strftime('%Y-%m-%d')
+                file.filename = "_".join(file.filename.split("_")[:3])
 
-                file.filename="_".join(file.filename.split("_")[:3])
-            
-                excel_buffer= BytesIO()
-                save_dfs_to_excel(df_logic_duplicate,df_perfect_duplicate,df_missing_relationship,excel_buffer)
-
-
-                zip_buffer=BytesIO()
+                # Initialiser le fichier ZIP
+                zip_buffer = BytesIO()
                 with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED, False) as zip_file:
-                    # Ajouter les fichiers CSV et excel dans le fichier ZIP en utilisant les noms de fichiers construits
-                    zip_file.writestr(f"Logic_duplicate_{file.filename}_{today}.csv", df_logic_duplicate.to_csv(index=False, encoding="utf-8", sep=";"))
-                    zip_file.writestr(f"Perfect_duplicate_{file.filename}_{today}.csv", df_perfect_duplicate.to_csv(index=False, encoding="utf-8", sep=";"))
-                    zip_file.writestr(f"Missing_relationship_{file.filename}_{today}.csv", df_missing_relationship.to_csv(index=False, encoding="utf-8", sep=";"))
-                    zip_file.writestr(f'ALL_Errors_report_{file.filename}_{today}.xlsx', excel_buffer.getvalue())
+                    # Filtrer les données par Manufacturer
+                    grouped = df.groupby('Manufacturer')
+
+                    for manufacturer, group_df in grouped:
+                        # Créer des DataFrames filtrés
+                        df_logic_duplicate = group_df[group_df['ErrorType'].str.startswith('Logical Duplicate with')]
+                        df_perfect_duplicate = group_df[group_df['ErrorType'].str.startswith('Perfect Duplicate with')]
+                        df_missing_relationship = group_df[group_df['ErrorType'].str.startswith('Missing relationship')]
+
+                        # Ajouter les colonnes et supprimer les colonnes inutiles
+                        df_logic_duplicate = add_columns_and_remove(df_logic_duplicate)
+                        df_perfect_duplicate = add_columns_and_remove(df_perfect_duplicate)
+                        df_missing_relationship = add_columns_and_remove(df_missing_relationship)
+
+                        # Classifier et traduire les types d'erreurs
+                        df_missing_relationship = sort_missing_relationships(df_missing_relationship.copy())
+                        df_missing_relationship = keep_first_occurrence_for_missing_relationship(df_missing_relationship, "ParentId")
+                        df_missing_relationship = modify_error_type(df_missing_relationship)
+
+                        # Sauvegarder dans Excel
+                        excel_buffer = BytesIO()
+                        save_dfs_to_excel(df_logic_duplicate, df_perfect_duplicate, df_missing_relationship, excel_buffer)
+
+                        # Ajouter les fichiers dans le dossier du Manufacturer
+                        folder_name = f"{manufacturer}/"
+                        zip_file.writestr(f"{folder_name}Logic_duplicate_{file.filename}_{today}.csv", df_logic_duplicate.to_csv(index=False, encoding="utf-8", sep=";"))
+                        zip_file.writestr(f"{folder_name}Perfect_duplicate_{file.filename}_{today}.csv", df_perfect_duplicate.to_csv(index=False, encoding="utf-8", sep=";"))
+                        zip_file.writestr(f"{folder_name}Missing_relationship_{file.filename}_{today}.csv", df_missing_relationship.to_csv(index=False, encoding="utf-8", sep=";"))
+                        zip_file.writestr(f"{folder_name}ALL_Errors_report_{file.filename}_{today}.xlsx", excel_buffer.getvalue())
+
+                    # Ajouter le fichier KPI global (pas spécifique à un Manufacturer)
                     zip_file.writestr(f"KPI_{file.filename}_{today}.csv", df_kpi.to_csv(index=False, encoding="utf-8", sep=";"))
 
                 zip_buffer.seek(0)
-                # Retourner le fichier ZIP en tant que réponse à la requête POST
+                # Retourner le fichier ZIP en tant que réponse
                 return send_file(
                     zip_buffer,
                     as_attachment=True,
                     mimetype='application/zip',
                     download_name=f'filtered_data_{file.filename}.zip'
-                    )
+                )
             except Exception as e:
-              return render_template('upload.html', error_message=f'Erreur lors du traitement du fichier : {str(e)}')
+                return render_template('upload.html', error_message=f'Erreur lors du traitement du fichier : {str(e)}')
         else:
             return render_template('upload.html', error_message='Extension de fichier non autorisée.')
     return render_template('upload.html')
